595cc72688639ccd40118d2aa36149877d8fbc12
Percy-Coding <55159839+Percy-Coding@users.noreply.github.com>
2019-10-29 05:31:43 -0500

    Add files via upload

--- Migrations/20191029061311_Initial.Designer.cs
+++ Migrations/20191029061311_Initial.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebProyecto.Data;
 
 namespace WebProyecto.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20191029061311_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
